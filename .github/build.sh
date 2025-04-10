ba59d2bd22ac82b58d227bba1de4cac13a735e7e
Lathika00 <bklathika@gmail.com>
2025-04-10 18:49:40 +0530

    Create build.sh

---
+++ .github/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is a build script example
+
+echo "Starting the build process..."
+
+# Add your build commands here
+echo "Compiling source code..."
+# Example: gcc -o my_program source_code.c
+
+echo "Build process completed!"
